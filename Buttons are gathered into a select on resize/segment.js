39e19f344f0cfb6f518e6711bd25d861f6495101
xena <zizu087@gmail.com>
2018-12-10 19:15:34 -0800

    Connected the selected button with a select option

---
+++ Buttons are gathered into a select on resize/segment.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function() {
+
+  var buttons = document.querySelectorAll('button');
+  var select = document.querySelector('select');
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function (){
+      buttons.forEach(function (item) {
+        item.style.backgroundColor = 'white';
+      });
+      this.style.backgroundColor = 'pink';
+      select.selectedIndex = this.value - 1;
+    });
+  }
+
+})();
